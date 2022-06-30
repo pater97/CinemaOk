7470affa2097d9795b20243822fc2d474bf67959
Giuseppe Paternostro <giuseppe.paternostro@vidiemme.it>
2022-06-30 13:06:17 +0200

    completata sezione search

--- public/js/search.js
+++ public/js/search.js
@@ -1,43 +1,63 @@
-//fare in modo che se seleziono serie o movie la stinga api cambi variabile
-//inniettare i film tramite javascript
-
 let searchInput = document.getElementById('searchInput')
 const container = document.getElementById('results')
+let selectType = document.getElementById('type')
 const api = 'https://api.themoviedb.org/3/search/'
 const apiKey = 'api_key=f4f382566368553eb6b723d584f2ef40'
 
-let movies = []
-
 function getMovie() {
-  // pulisco l'array dalle chiamate fatte in precedenza
-  movies = []
+  console.log(selectType.value)
+  container.innerHTML = ''
   axios
     .get(
       `
-    ${api}movie?${apiKey}&language=en-US&query=${searchInput.value}&page=1&include_adult=false`
+    ${api}${selectType.value}?${apiKey}&language=en-US&query=${searchInput.value}&page=1&include_adult=false`
     )
     .then((response) => {
-      container.innerHTML = ''
       const singleObject = response.data.results
-      for (let i = 0; i < singleObject.length; i++) {
-        container.insertAdjacentHTML(
-          'beforeend',
+      if (singleObject[0].original_title === undefined) {
+        for (let i = 0; i < singleObject.length; i++) {
+          container.insertAdjacentHTML(
+            'beforeend',
+            `
+          <article class="card_container">
+              <div class="card_content">
+                <a href="/series/${singleObject[i].id}">
+                  <img src="${noimage(singleObject[i].poster_path)}" alt="">
+                  <h2>${singleObject[i].original_name}</h2>
+                </a>
+              </div>
+          </article>
           `
-        <article class="card_container">
-            <div class="card_content">
-              <a href="/movies/${singleObject[i].id}">
-                <img src="https://image.tmdb.org/t/p/original/${singleObject[i].poster_path}" alt="">
-                <h2>${singleObject[i].original_title}</h2>
-              </a>
-            </div>
-        </article>
-        `
-        )
-        movies.push(singleObject[i])
+          )
+          console.log(singleObject[i].original_title)
+        }
+      } else {
+        for (let i = 0; i < singleObject.length; i++) {
+          container.insertAdjacentHTML(
+            'beforeend',
+            `
+          <article class="card_container">
+              <div class="card_content">
+                <a href="/movies/${singleObject[i].id}">
+                  <img src="${noimage(singleObject[i].poster_path)}" alt="">
+                  <h2>${singleObject[i].original_title}</h2>
+                </a>
+              </div>
+          </article>
+          `
+          )
+          console.log(singleObject[i].original_title)
+        }
       }
-      console.log(movies)
     })
     .catch((e) => {
       console.log(e)
     })
+}
+
+function noimage(image) {
+  if (image === null) {
+    return 'https://preview.redd.it/z9xed4f4guw71.jpg?auto=webp&s=7e7cd9550853af5caa7a3f805b1f8edc11f59fe1'
+  }
+  return `https://image.tmdb.org/t/p/original/${image}`
 }
